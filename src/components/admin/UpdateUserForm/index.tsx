b662e6787c674335db630d3c0ee8af8236f5dad4
vinicius <viniciusfernandesoliveira8@gmail.com>
2025-12-06 16:57:31 -0300

    feat: add delete user action and use it on form

--- src/components/admin/UpdateUserForm/index.tsx
+++ src/components/admin/UpdateUserForm/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { deleteUserAction } from "@/actions/user/delete-user-action";
 import { updateUserAction } from "@/actions/user/update-user-action";
 import { Button } from "@/components/Button";
 import { Dialog } from "@/components/Dialog";
@@ -35,7 +36,20 @@ export function UpdateUserForm({ user }: Props) {
     });
   }, [])
 
-  function handleDeleteUserAccount() {}
+  function handleDeleteUserAccount() {
+    startTransition(async () => {
+      if (!confirm('Confirma só mais uma vez que quer continuar')) return;
+
+      const result = await deleteUserAction();
+
+      if (result.errors) {
+        toast.dismiss();
+        result.errors.forEach(e => toast.error(e));
+      }
+
+      setIsDialogVisible(false);
+    });
+  }
 
   useEffect(() => {
     toast.dismiss();
